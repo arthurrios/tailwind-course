f20df9e04ee7c3324336a2411095231d92540a53
arthur <arthur.rios007@gmail.com>
2023-10-16 14:34:04 -0300

    img context and preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
             </label>
             <FileInput.Root>
               <FileInput.Trigger />
-              <FileInput.Control />
+              <FileInput.Control multiple />
             </FileInput.Root>
           </div>
 
